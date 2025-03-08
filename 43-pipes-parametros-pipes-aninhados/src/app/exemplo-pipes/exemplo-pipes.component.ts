e1ddfcac7bc057bcda657825803109639d43eeec
gabznavas <gabznavas@proton.me>
2025-03-08 09:23:19 -0300

    pipes

---
+++ 43-pipes-parametros-pipes-aninhados/src/app/exemplo-pipes/exemplo-pipes.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo-pipes',
+  templateUrl: './exemplo-pipes.component.html',
+  styleUrl: './exemplo-pipes.component.scss'
+})
+export class ExemploPipesComponent {
+  book = {
+    title: 'Learning JavaSCript Data Sctructures and Algorithms 2nd...',
+    rating: 4.54321,
+    pageCount: 314,
+    price: 44.99,
+    launch: new Date(2016, 5,23),
+    url: 'http://a.co/glqjpRP'
+  }
+}
