3bd9d242d8f2d3da947be490a3c4a56c4b48f6e3
gabznavas <gabznavas@proton.me>
2025-03-08 15:44:42 -0300

    changed

--- 53-definindo-parametros-roteamento/src/app/app-routing.module.ts
+++ 53-definindo-parametros-roteamento/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursosComponent } from './cursos/cursos.component';
 
 const routes: Routes = [{
   path: '',
@@ -10,10 +9,7 @@ const routes: Routes = [{
 },{
   path: 'curso/:id',
   component: CursoDetalheComponent
-},{
-  path: 'cursos',
-  component: CursosComponent
-}];
+},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
