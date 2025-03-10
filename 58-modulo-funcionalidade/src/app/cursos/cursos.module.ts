af6d9173ebc2610ab1e84bd96c53fe6a1c137ad0
gabznavas <gabznavas@proton.me>
2025-03-09 22:37:42 -0300

    refactory created module cursos

---
+++ 58-modulo-funcionalidade/src/app/cursos/cursos.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { CursosComponent } from "./cursos.component";
+import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
+import { CursoNaoEncontradoComponent } from "./curso-nao-encontrado/curso-nao-encontrado.component";
+import { CursosService } from "./services/cursos.service";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [],
+  declarations: [
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent,
+  ],
+  providers: [
+    CursosService
+  ],
+  
+})
+export class CursosModule {}
