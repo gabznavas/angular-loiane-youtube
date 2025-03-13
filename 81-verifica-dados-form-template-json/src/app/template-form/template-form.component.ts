08a9f9310f14d139e9bea42c3aec25bfaa729712
gabznavas <gabznavas@proton.me>
2025-03-13 19:30:26 -0300

    forum debug component

---
+++ 81-verifica-dados-form-template-json/src/app/template-form/template-form.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Form, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrl: './template-form.component.scss'
+})
+export class TemplateFormComponent {
+
+  usuario = {
+    nome: null,
+    email: null
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    console.log(form.value);
+    console.log(this.usuario);
+    console.log(form.control.invalid);
+    console.log(form.errors);
+
+  }
+}
