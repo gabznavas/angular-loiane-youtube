c932613350c5589de3ac062cbc46d32d8609cd4d
gabznavas <gabznavas@proton.me>
2025-04-11 08:36:17 -0300

    form reactivos configuracoes

---
+++ 89-form-reativos-configuracoes/src/app/data-form/data-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-form',
+  templateUrl: './data-form.component.html',
+  styleUrl: './data-form.component.scss'
+})
+export class DataFormComponent {
+}
